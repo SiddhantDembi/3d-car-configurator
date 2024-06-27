0cc3edc5a0b30d7c5f1b99a97129f32b8981e911
SiddhantDembi <siddhantdembi@gmail.com>
2024-06-27 14:37:47 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head> <link rel="icon" href="/favicon.ico" type="image/x-icon" /></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
